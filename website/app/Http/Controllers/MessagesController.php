cd37ace249e06c183131f546b2f0ee3bfee5ae9b
Unknown <millkamill@hotmail.com>
2018-08-05 14:18:18 +0200

    Form controller

--- website/app/Http/Controllers/MessagesController.php
+++ website/app/Http/Controllers/MessagesController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    public function submit(){
-        return 123;
+    public function submit(Request $request){
+        $this->validate($request, [
+            'name'=>'required',
+            'email'=>'required',
+        ]);
+
+        return "SUCCESS!";
     }
 }
