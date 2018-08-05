930483f29bb3b709d3b0c9d5a902d13384367b37
Unknown <millkamill@hotmail.com>
2018-08-05 14:51:43 +0200

    enter messages to the database

--- website/app/Http/Controllers/MessagesController.php
+++ website/app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -12,6 +13,15 @@ class MessagesController extends Controller
             'email'=>'required',
         ]);
 
-        return "SUCCESS!";
+    // Create new message
+    $message = new Message;
+    $message->name = $request->input('name');
+    $message->email = $request->input('email');
+    $message->message = $request->input('message');
+    // Save message
+    $message->save();
+
+    //Redirect
+    return redirect('home')->with('success', 'Your message has been sent');
     }
 }
